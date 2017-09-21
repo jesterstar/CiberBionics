11e9ef3273cb81a8c24ea9af4bf58f7a960c15c5
sergey.glazov <sergey.glazov@kitsoft.kiev.ua>
2017-09-21 17:58:40 +0300

    003-005 not ready yet

---
+++ 003/005/common.js
@@ -0,0 +1,24 @@
+function getResult(id, result) {
+	var el = document.getElementById(id);
+	var resultContainer = document.getElementById(result);
+
+	return function () {
+		var newParagraph = document.createElement('p');
+		var str = el.value;
+		newParagraph.innerHTML = str;
+
+		if (str != null && str.length) {
+			resultContainer.appendChild(newParagraph);
+		}
+	}
+}
+
+var button = document.getElementById('button');
+var generateResult = getResult('input', 'result');
+
+//Example click listener
+button.addEventListener(
+	'click',
+	generateResult,
+	false);
+
