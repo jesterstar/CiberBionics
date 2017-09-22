f8f068858d1887dc0dd519d898631223780a064c
sergey.glazov <sergey.glazov@kitsoft.kiev.ua>
2017-09-22 11:36:16 +0300

    004-001

---
+++ 004/001/script.js
@@ -0,0 +1,41 @@
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * ((max + 1) - min)) + min;
+}
+
+function isNumeric(n) {
+	return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function randomizer(id) {
+	var input = document.getElementById(id);
+	var resultContainer = document.getElementById('result-list');
+
+	return function () {
+		console.log(isNumeric(input.value));
+		if (input.value.length > 0 && isNumeric(input.value)) {
+			var number = getRandomInt(1, 100);
+			if (input.value < number) {
+				resultContainer.innerHTML = 'загаданное значение больше введенного вами, загаданое = ' + number + '; введенное = ' + input.value;
+			} else if (input.value > number) {
+				resultContainer.innerHTML = 'загаданное значение меньше введенного вами, загаданое = ' + number + '; введенное = ' + input.value;
+			} else if (input.value == number) {
+				resultContainer.innerHTML = 'загаданое = введенному = ' + input.value;
+			} else if (input.value < 0 && input.value > 100) {
+				resultContainer.innerHTML = 'диапазон чисел 1 - 100';
+			}
+		} else if (!isNumeric(input.value)) {
+			resultContainer.innerHTML = 'введите число';
+		} else {
+			resultContainer.innerHTML = 'вы не заполнили поле';
+		}
+	}
+}
+
+var button = document.getElementById('button');
+var generateResult = randomizer('exampleInput');
+
+//Example click listener
+button.addEventListener(
+	'click',
+	generateResult,
+	false);
